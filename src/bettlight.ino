9556e74e8c8b806dc060870b984ff47f32f79f85
Armin F. Gnosa <afg@alarmschaben.de>
2016-10-15 21:54:36 +0200

    implemented button press detection plus some variable renaming

--- src/bettlight.ino
+++ src/bettlight.ino
@@ -1,4 +1,10 @@
 class LightedButton {
+    public:
+        enum ButtonPressState {
+            NONE,
+            SHORT,
+            LONG};
+
     private:
         // method variables
         int buttonPin;
@@ -9,17 +15,30 @@ class LightedButton {
         uint8_t maxBrightness = 254;
         uint16_t fullyLitDuration = 2000;
 
+        uint16_t shortPressMinDuration = 100;
+        uint16_t longPressMinDuration = 800;
+
+        // FSM declarations
+        enum ButtonFSMState {
+            BTN_IDLE,
+            BTN_SHORT_PRESS,
+            BTN_LONG_PRESS};
+
+        enum ButtonLEDFSMstate {
+            LED_IDLE,
+            LED_FADE_UP,
+            LED_FULLY_LIT,
+            LED_FADE_DOWN};
+
         // runtime variables
         uint8_t currentBrightness;
 
-        enum ButtonLEDstate {
-            IDLE,
-            FADE_UP,
-            FULLY_LIT,
-            FADE_DOWN};
+        ButtonFSMState bs;
+        ButtonPressState bps;
+        ButtonLEDFSMstate bls;
 
-        ButtonLEDstate bls;
-        uint32_t lastStateChangeMillis;
+        uint32_t lastLEDStateChangeMillis;
+        uint32_t lastButtonStateChangeMillis;
 
     public:
         LightedButton(int buttonPin, int ledPin) {
@@ -31,49 +50,101 @@ class LightedButton {
 
             pinMode(ledPin, OUTPUT);
             analogWrite(ledPin, idleBrightness);
+
+            bs = BTN_IDLE;
+            bps = NONE;
+            bls = LED_IDLE;
         }
 
+        // forward declarations
         void doFSM();
+        void doButtonFSM();
+        void doLEDFSM();
+        ButtonPressState getButtonPress();
+        void clearButtonPress();
 };
 
 void LightedButton::doFSM() {
+    doButtonFSM();
+    doLEDFSM();
+}
+
+void LightedButton::doButtonFSM() {
+    switch(bs) {
+        case BTN_IDLE:
+            if (!digitalRead(buttonPin) && (millis() - lastButtonStateChangeMillis >= shortPressMinDuration)) {
+                bs = BTN_SHORT_PRESS;
+                lastButtonStateChangeMillis = millis();
+            }
+            else if (digitalRead(buttonPin)) {
+                lastButtonStateChangeMillis = millis();
+            }
+            break;
+        case BTN_SHORT_PRESS:
+            if (!digitalRead(buttonPin)) {
+                if (millis() - lastButtonStateChangeMillis >= longPressMinDuration) {
+                    bs = BTN_LONG_PRESS;
+                }
+            } else {
+                bs = BTN_IDLE;
+                bps = SHORT;
+            }
+            break;
+        case BTN_LONG_PRESS:
+            if (digitalRead(buttonPin)) {
+                bs = BTN_IDLE;
+                bps = LONG;
+            }
+            break;
+    }
+}
+
+LightedButton::ButtonPressState LightedButton::getButtonPress() {
+    return bps;
+}
+
+void LightedButton::clearButtonPress() {
+    bps = NONE;
+}
+
+void LightedButton::doLEDFSM() {
     switch(bls) {
-        case IDLE:
+        case LED_IDLE:
             currentBrightness = idleBrightness;
             if (!digitalRead(buttonPin)) {
-                bls = FADE_UP;
-                lastStateChangeMillis = millis();
+                bls = LED_FADE_UP;
+                lastLEDStateChangeMillis = millis();
             }
             break;
-        case FADE_UP:
-            if (millis() - lastStateChangeMillis >= 1) {
+        case LED_FADE_UP:
+            if (millis() - lastLEDStateChangeMillis >= 1) {
                 currentBrightness += 2;
-                lastStateChangeMillis = millis();
+                lastLEDStateChangeMillis = millis();
             }
             if (currentBrightness >= maxBrightness) {
-                bls = FULLY_LIT;
-                lastStateChangeMillis = millis();
+                bls = LED_FULLY_LIT;
+                lastLEDStateChangeMillis = millis();
             }
             break;
-        case FULLY_LIT:
+        case LED_FULLY_LIT:
             currentBrightness=maxBrightness;
             if (!digitalRead(buttonPin))
-                lastStateChangeMillis = millis();
-            if (millis() - lastStateChangeMillis >= fullyLitDuration) {
-                bls = FADE_DOWN;
-                lastStateChangeMillis = millis();
+                lastLEDStateChangeMillis = millis();
+            if (millis() - lastLEDStateChangeMillis >= fullyLitDuration) {
+                bls = LED_FADE_DOWN;
+                lastLEDStateChangeMillis = millis();
             }
             break;
-        case FADE_DOWN:
-            if (millis() - lastStateChangeMillis >= 1) {
+        case LED_FADE_DOWN:
+            if (millis() - lastLEDStateChangeMillis >= 1) {
                 currentBrightness -= 2;
-                lastStateChangeMillis = millis();
+                lastLEDStateChangeMillis = millis();
             }
             if (currentBrightness <= idleBrightness) {
-                bls = IDLE;
-                lastStateChangeMillis = millis();
+                bls = LED_IDLE;
+                lastLEDStateChangeMillis = millis();
             }
-            lastStateChangeMillis = millis();
+            lastLEDStateChangeMillis = millis();
             break;
     }
     analogWrite(ledPin, currentBrightness);
